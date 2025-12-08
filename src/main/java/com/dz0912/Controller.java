e03e25197b566a0a285e2fa5ad99139a280cde17
Neiromen <alex.kor0702@gmail.com>
2025-12-08 23:34:41 +0300

    curls added

--- src/main/java/com/dz0912/Controller.java
+++ src/main/java/com/dz0912/Controller.java
@@ -10,12 +10,14 @@ public class Controller {
 
     private List<Theme> themes = new ArrayList<>();
 
+    // curl -X POST http://localhost:8080/theme -H "Content-Type: application/json" -d "\"Новая тема\""
     @PostMapping("/theme")
     public ResponseEntity<String> createTheme(@RequestBody String title) {
         themes.add(new Theme(title));
         return ResponseEntity.ok(title);
     }
 
+    // curl -X DELETE http://localhost:8080/theme/0
     @DeleteMapping("/theme/{id}")
     public ResponseEntity<Void> deleteTheme(@PathVariable int id) {
         if (id < 0 || id >= themes.size())
@@ -26,6 +28,9 @@ public class Controller {
         return ResponseEntity.noContent().build();
     }
 
+    // curl http://localhost:8080/themes
+    // curl http://localhost:8080/themes?sortOrder=minToMax
+    // curl http://localhost:8080/themes?sortOrder=maxToMin
     @GetMapping("/themes")
     public ResponseEntity<List<Theme>> getAllThemes(@RequestParam(required = false) String sortOrder) {
         if (sortOrder == null || sortOrder.equals("minToMax")) {
@@ -36,6 +41,7 @@ public class Controller {
         return ResponseEntity.noContent().build();
     }
 
+    // curl -X PUT http://localhost:8080/theme/0 -H "Content-Type: application/json" -d "\"Обновленная тема\""
     @PutMapping("/theme/{id}")
     public ResponseEntity<String > updateTheme(@RequestBody String title, @PathVariable int id) {
         if (id < 0 || id >= themes.size())
@@ -46,22 +52,30 @@ public class Controller {
         return ResponseEntity.ok(title);
     }
 
+    // curl http://localhost:8080/themes/length
     @GetMapping("/themes/length")
     public ResponseEntity<Integer> getAllThemesLength() {
         return ResponseEntity.ok(themes.size());
     }
 
+    // curl -X DELETE http://localhost:8080/themes/all
     @DeleteMapping("/themes/all")
     public ResponseEntity<Void> deleteAllThemes() {
         themes.clear();
         return ResponseEntity.noContent().build();
     }
 
+    // curl http://localhost:8080/theme/0
     @GetMapping("/theme/{id}")
     public ResponseEntity<Theme> getTheme(@PathVariable int id) {
         return ResponseEntity.ok(themes.get(id));
     }
 
+    /*
+    curl -X POST http://localhost:8080/theme/0/comment \
+    -H "Content-Type: application/json" \
+    -d '{"username": "user1", "text": "Мой комментарий"}'
+    */
     @PostMapping("/theme/{id}/comment")
     public ResponseEntity<Void> createComment(@RequestBody Comment comment, @PathVariable int id) {
         if (id < 0 || id >= themes.size())
@@ -74,6 +88,7 @@ public class Controller {
         return ResponseEntity.ok().build();
     }
 
+    // curl -X DELETE http://localhost:8080/theme/0/comment/0
     @DeleteMapping("/theme/{themeId}/comment/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable int themeId, @PathVariable int commentId) {
         if (themeId < 0 || themeId >= themes.size())
@@ -90,6 +105,11 @@ public class Controller {
         return ResponseEntity.ok().build();
     }
 
+    /*
+    curl -X PUT http://localhost:8080/theme/0/comment/0 \
+    -H "Content-Type: application/json" \
+    -d '{"username": "user1", "text": "Обновленный комментарий"}'
+    */
     @PutMapping("/theme/{themeId}/comment/{commentId}")
     public ResponseEntity<Void> updateComment(@RequestBody Comment comment,
                                               @PathVariable int commentId,
@@ -109,6 +129,9 @@ public class Controller {
         return ResponseEntity.ok().build();
     }
 
+    // curl http://localhost:8080/theme/0/comments
+    // curl http://localhost:8080/theme/0/comments?sortOrder=minToMax
+    // curl http://localhost:8080/theme/0/comments?sortOrder=maxToMin
     @GetMapping("/theme/{themeId}/comments")
     public ResponseEntity<List<Comment>> getAllComments(@PathVariable int themeId,
                                                         @RequestParam(required = false) String sortOrder) {
@@ -125,6 +148,9 @@ public class Controller {
         return ResponseEntity.noContent().build();
     }
 
+    // curl http://localhost:8080/comments/user1
+    // curl http://localhost:8080/comments/user1?sortOrder=minToMax
+    // curl http://localhost:8080/comments/user1?sortOrder=maxToMin
     @GetMapping("/comments/{username}")
     public ResponseEntity<List<Comment>> getAllCommentsByUsername(@PathVariable String username,
                                                                   @RequestParam(required = false) String sortOrder) {
@@ -143,6 +169,7 @@ public class Controller {
         }
         return ResponseEntity.noContent().build();    }
 
+    // curl -X DELETE http://localhost:8080/comments/user1
     @DeleteMapping("/comments/{username}")
     public ResponseEntity<Void> deleteAllCommentsByUsername(@PathVariable String username) {
         for(Theme theme : themes) {
